b832f5b585989787ef61131dd46086374d842553
sidkrish03 <ss3875@njit.edu>
2021-06-17 18:53:49 -0500

    Added more forecast information

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -20,7 +20,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error) { // Bad coordinates
             callback('Unable to find location! Please try again', undefined) 
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.'
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity level is currently '
+            + body.current.humidity + '%.'
             )
         }
     })
